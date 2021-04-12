4e2b9a66f787f3372ad8b3419b66dbc04fa85dc5
Mariam_Yusra21 <memon.yusra21@gmail.com>
2021-04-11 18:20:25 -0700

    no message

--- SwapVariable.java
+++ SwapVariable.java
@@ -3,7 +3,7 @@ public class SwapVariable {
     public static void main(String[] args) {
         Scanner number = new Scanner(System.in);
         int num1 , num2;
-        System.out.println("For Swappingbthe Variables you should give the integer data to the Variables...");
+        System.out.println("For Swapping the Variables you should give the integer data to the Variables...");
         System.out.print("Enter your Data for first Variable: ");
         num1 = number.nextInt();
         System.out.print("Enter your Data for second Variable: ");
@@ -14,8 +14,8 @@ public class SwapVariable {
         num2 = num1 - num2;
         num1 = num1 - num2;
         System.out.println("Now the data of Variables are changed...");
-        System.out.println("The data of your first Variable has been changed to" + num2);
-        System.out.println("The data of your second Variable has been changed to" + num1);
+        System.out.println("The data of your first Variable has been changed to" + num1);
+        System.out.println("The data of your second Variable has been changed to" + num2);
 
         number.close();
     }
