297a0fe18ca11dd9b697523d8f0cfa62abf3f078
Mariam_Yusra21 <memon.yusra21@gmail.com>
2021-04-24 11:23:47 +0500

    Task #2b [24 April 2021] (Basic attributes of class Accounts).

---
+++ Account.java
@@ -0,0 +1,9 @@
+import java.util.Date;
+public class Account {
+    String firstName = "Mariam";
+    String lastName = "Yusra";
+    String email = "memon.yusra21@gmail.com"; 
+    private String password = "**********";
+    Date dateOfJoin = new Date(105, 2, 22, 5, 25); 
+
+}
