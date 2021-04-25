bbf3af1ca6e97c6bd1e74b3e7355a45f7a7aca60
Mariam_Yusra21 <memon.yusra21@gmail.com>
2021-04-25 17:33:12 +0500

    Task # 3.1 [25/April/2021] (Arthimetic Operations Test).

---
+++ Pratice/ArithmaticOperations.java
@@ -0,0 +1,21 @@
+public class ArithmaticOperations {
+    public static void main(String[] args) {
+        short x = 6;
+        int y = 4;
+        float a = 12.5f;
+        float b = 7f;
+        
+        System.out.println("X is: " + x + " , Y is: " + y);
+        System.out.println();
+
+        System.out.println("The operation on integer numbers are:");
+        System.out.println("X + Y = " + (x + y));
+        System.out.println("X - Y = " + (x - y));
+        System.out.println("X / Y = " + (x / y));
+        System.out.println("X mod Y = " + (x % y));
+
+        System.out.println("The operation on float numbers are:");
+        System.out.println("A is: " + a + " , B is: " + b);
+        System.out.println("A / B = " + (a / b));
+    }    
+}
