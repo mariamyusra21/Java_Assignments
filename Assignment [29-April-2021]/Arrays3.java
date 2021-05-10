4531afcf74ef5ded2e1c8d5b3688be96ba24c504
Mariam_Yusra21 <memon.yusra21@gmail.com>
2021-05-10 17:30:56 +0500

    Implemented Bubble sort in the Concatenated Arrays [Arrays3.java]

--- Assignment [29-April-2021]/Arrays3.java
+++ Assignment [29-April-2021]/Arrays3.java
@@ -18,13 +18,34 @@ public class Arrays3 {
         return array3;
     }
 
+    // Bubble Sort of the Array...
+    static int[] sortArray(int[] array){
+        int n = array.length;
+        for (int i = 0; i < n-1; i++) {                 // Outer Loop
+            int ptr = 0;
+            while (ptr < n-i-1) {                       // Inner Loop
+                if (array[ptr] > array[ptr + 1]) {
+                    int temp = array[ptr];
+                    array[ptr] = array[ptr + 1];
+                    array[ptr + 1] = temp;
+                }
+                ptr++;
+            }
+        }
+
+        return array;
+    }
+
+
     public static void main(String[] args) {
-        int[] myArray1 = {4,5,6};
-        int[] myArray2 = {7,8,9};
+        int[] myArray1 = {2,4,6,8,10};
+        int[] myArray2 = {1,3,5,7,9};
 
         System.out.println("The Concatenation of " + Arrays.toString(myArray1) +" and " + Arrays.toString(myArray2) + " is:");
         int[] myArray3 = addArrays(myArray1, myArray2);
         System.out.println(Arrays.toString(myArray3));
+        int[] sortedArray = sortArray(myArray3);
+        System.out.println("Bubble sort of the Array is " + Arrays.toString(sortedArray));
 
     }
 }
